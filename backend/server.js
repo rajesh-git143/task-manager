7d935a71d960901d83dfb374490b3c79bfdf279d
raj <raju19932705@gmail.com>
2025-08-23 01:56:07 -0400

    Setup Express backend and MongoDB connection [TM-2]

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,29 @@
-"console/log('server running..')"
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+
+//Middleware
+
+app.use(cors());
+app.use(express.json());
+
+//MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("Mongodb Connected"))
+.catch((err)=> console.error("MongoDB error:",err));
+
+//Default route
+
+app.get('/',(req,res)=>{
+    res.send('Task Manager API is running...');
+});
+
+
+//start server
+
+const PORT = process.env.PORT||5000;
+app.listen(PORT,()=>console.log("server is running on port ${PORT}"));
